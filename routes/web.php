73e42dddbba19cf58a9ce56256a02439bd3fe9ed
B00143361 Amos Fakrogha <B00143361@mytudublin.ie>
2024-02-12 16:40:57 +0000

    lab 1 is done

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,15 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+|Route::get('/customers/new', 'App\Http\Controllers\CustomerController@new');
+Route::post('/customers/create', 'App\Http\Controllers\CustomerController@create')->name('customers.create'); 
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/customers/new', 'App\Http\Controllers\CustomerController@new');
+Route::post('/customers/create', 'App\Http\Controllers\CustomerController@create')->name('customers.create'); 
+
+Route::resource('members', App\Http\Controllers\memberController::class);
